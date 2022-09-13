a3cf4bf3c379394fdc25f731f64c31644a9837f6
Marc-André tétrault <marc-andre.tetrault@usherbrooke.ca>
2022-09-13 08:57:40 -0400

    Ajout d'un exemple plus générique

--- verif/core/templates/ComponentBuilder_Template.py
+++ verif/core/templates/ComponentBuilder_Template.py
@@ -15,6 +15,7 @@ class TestbenchClass_WithOnlyScoreboard:
 
     def BuildVerificationComponents(self):
         self.BuildVerificationComponents_MergerCRC8()
+        # self.BuildVerificationComponents_OtherModule() # generic example for component group
 
     # Divide and conquer - One scoreboard per member function
     # Building is done in this order:
@@ -45,6 +46,34 @@ class TestbenchClass_WithOnlyScoreboard:
         # Use cocotb scoreboard class. Instanciate, then connect/add_interface
         # with 1-monitor object and 2- model result
         # Pretty magical (nothing to do except build it).
-        self.inst_MergerCRC8_scoreboard = Scoreboard(self._dut) #, fail_immediately=False)
+        self.inst_MergerCRC8_scoreboard = Scoreboard(self._dut, fail_immediately=False)
         self.inst_MergerCRC8_scoreboard.name = "inst_CRC8_scoreboard"
         self.inst_MergerCRC8_scoreboard.add_interface(self.inst_MergerCRC8_monitor, self.inst_MergerCRC8_model.expected_output)
+
+
+    def BuildVerificationComponents_OtherModule(self):
+        #from ???   import Monitor1_Class
+        #from ???   import Model_Class
+        #from ???   import Monitor2_Class
+
+        # Instanciate and initialize Monitor
+        self.inst_monitor1 = Monitor1_Class(self._dut.inst_name1,
+                                                    logging=self._logging)
+        self.inst_monitor1._name = "inst_monitor1"
+
+        # Instanciate model before monitor that feeds it.
+        self.inst_model = Model_Class()
+        self.inst_model.name = "inst_model"
+
+        # Instanciate and initialize other monitor. Link with model through callback
+        self.inst_monitor2 = Monitor2_Class(self._dut.inst_name2,
+                                            logging=self._logging,
+                                            callback=self.inst_model.callback)
+        self.inst_monitor2._name = "inst_monitor2"
+
+        # Use cocotb scoreboard class. Instanciate, then connect/add_interface
+        # with 1-monitor object and 2- model result
+        # Pretty magical (nothing to do except build it).
+        self.inst_scoreboard = Scoreboard(self._dut) #, fail_immediately=False)
+        self.inst_scoreboard.name = "inst_scoreboard"
+        self.inst_scoreboard.add_interface(self.inst_monitor1, self.inst_model.model_result)
