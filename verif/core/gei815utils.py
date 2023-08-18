dcfaec852b2a63f5a6f7325c78b0bd32c24f24b6
Marc-André Tétrault <marc-andre.tetrault@usherbrooke.ca>
2023-08-18 15:16:31 -0400

    Update gei815utils.py. expanded comments on usage, updated prefix for function get expected

--- verif/core/gei815utils.py
+++ verif/core/gei815utils.py
@@ -20,19 +20,16 @@ def gei815_build_command_message(command, addr, data):
     message = (command << 43) + (addr << 32) + data
     return cocotb.binary.BinaryValue(value=message, n_bits=48, bigEndian=False)
 
-"""
-CRC utility
-Usage example
-    SomeData = gei815_build_command_message(0, 9, 0x345678)  # Reading reg at address 0
-    resultingCRC = get_expected_crc(SomeData.buff)
-    crc_to_send = cocotb.binary.BinaryValue(value=resultingCRC, n_bits=8, bigEndian=False)
-    await uart_source.write(crc_to_send.buff)
-"""
+
 
 CRC8_START = 0x0D
 CRC_POLY = 0xC6
 
-def gei815_calculateCRC8(data, current_crc):
+"""
+CRC calculator, using past cumulative CRC (current_crc) and next bye (data).
+One byte per function call.
+"""
+def gei815_calculateCRC8_singleCycle(data, current_crc):
     crc = int2ba(current_crc, 8)
     data_bits = int2ba(data, 8)
     poly = int2ba(CRC_POLY, 8)
@@ -43,10 +40,22 @@ def gei815_calculateCRC8(data, current_crc):
             crc >>= 1
     return ba2int(crc)
 
-
+"""
+CRC utility
+Calculates CRC from n-byte packet.
+Usage example
+    # Build some n-byte string, for example a read command
+    SomeData = gei815_build_command_message(0, 4, 0x345678)  # Reading reg at address 0
+    # Calculate its CRC
+    resultingCRC = gei815_get_expected_crc(SomeData.buff)
+    # Convert to cocotb format
+    crc_to_send = cocotb.binary.BinaryValue(value=resultingCRC, n_bits=8, bigEndian=False)
+    # write to UART driver
+    await uart_source.write(crc_to_send.buff)
+"""
 def gei815_get_expected_crc(valueArray):
     current_crc = CRC8_START
     for b in valueArray:
-        current_crc = gei815_calculateCRC8(b, current_crc)
+        current_crc = gei815_calculateCRC8_singleCycle(b, current_crc)
 
     return current_crc
