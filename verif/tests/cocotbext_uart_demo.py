1f678d94437a51bbe8f375f7e0795c525422471a
Marc-André Tétrault <marc-andre.tetrault@usherbrooke.ca>
2024-08-15 16:59:28 -0400

    Mises a jour mineures - alignement avec guide étudiant

--- verif/tests/cocotbext_uart_demo.py
+++ verif/tests/cocotbext_uart_demo.py
@@ -3,6 +3,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import Join
 from cocotbext.uart import UartSource, UartSink
 from utilsVerif import print_cocotb_BinaryValue
+from cocotb.log import SimLog
 
 
 # Decorator to tell cocotb this function is a coroutine
@@ -10,36 +11,32 @@ from utilsVerif import print_cocotb_BinaryValue
 async def cocotbext_uart_demo(dut):
     print("Uart instance demo")
 
-    # L.E5 - Ajouter l'instanciation du moniteur
-    # inst_Uart8Monitor = Monitor_VotreNomDeClasse(dut.CheminVersPacketMerger, logging=True)
-    # inst_Uart8Monitor.name = "inst_Uart8Monitor" # optionnel
+    # L2.E1 - Ajouter l'instanciation du MMC
+    # inst_MMC_CRC8 = MMC_CRC8(dut.CheminVersPacketMergerCRC8)
+    # await inst_MMC_CRC8.start()
 
-    # L.E6 - Ajout des moniteurs, modèles et scoreboard
-    # VerifStuff = TestbenchClass_WithOnlyScoreboard(dut, logging=True)
-    # VerifStuff.BuildVerificationComponents()
-
-    # L.E4 - Ajouter l'initialisation des pattes d'entrée et de l'horloge
+    # L1.E4 - Ajouter l'initialisation des pattes d'entrée et de l'horloge
     # await votre_initialisation(dut) 
 
     # Driver and Sink for the dut UART RX/TX channels
     uart_driver = UartSource(dut.in_sig, baud=1000000, bits=8)
     uart_sink   = UartSink(dut.out_sig, baud=1000000, bits=8)
 
-    # L.E4 - Start thread for the monitoring function (not monitor class) for the expected UART response.
-    # Task_returnMessage =  cocotb.start_soon(wait_reply(dut, uart_sink))
+    # L1.E4 - Start thread for the reply function for the expected UART response.
+    # Task_returnMessage = await cocotb.start(wait_reply(dut, uart_sink))
 
     # Generate arbitrary value to send on the UART
     SomeValue = cocotb.binary.BinaryValue(value=0x1023456789ABDCEF, n_bits=64, bigEndian=False)
 
     # Print cocotb value demo function. Uncomment if desired.
-    # gei815_print_cocotb_BinaryValue(SomeValue)
+    # print_cocotb_BinaryValue(SomeValue)
 
     # Send arbitrary value, then wait for transaction to complete
     await uart_driver.write(SomeValue.buff)
     await uart_driver.wait()
 
-    # L.E4 ait for response to complete or for timeout
-    # await Join(Task_returnMessage)
+    # L1.E4 ait for response to complete or for timeout
+    # await Task_returnMessage
 
 
 # L.E4 function to wait for response message
@@ -47,13 +44,19 @@ async def wait_reply(dut, uart_sink):
 
     # Non-infinite wait loop. Throw cocotb exception if timeout is reached (to do)
     for x in range(0, 100):
-        if(uart_sink.count() >= 6):
+        if(uart_sink.count() >= 7): ## 6 octets du message + le CRC
             break;
         await cocotb.triggers.ClockCycles(dut.clk, 1000, rising=True)
 
-    if(x == 100):
+    if(x == 99):
         print("Timeout")
-        # todo: Throw cocotb exception
+        logger = SimLog("cocotb.Test")
+        logger.info("Timeout for wait reply")
+        raise RuntimeError("Timeout for wait reply")
+        # or use plain assert.
+        #assert False, "Timeout for wait reply"
+        return None
+
     else:
         # cocotbext-uart returns byteArray. Convert to bytes first, then to Binary value for uniformity.
         message_bytes = bytes(await uart_sink.read(count=6))
